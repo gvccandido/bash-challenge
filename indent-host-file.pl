d8a4a583a6d4bac0fbe713b09e74a931c7773547
Gabriel Vinicius Canzi Candido <gabriel.vccandido@gmail.com>
2020-02-08 18:36:14 +0000

    Indent every field to the max size found on file
    So far only the IP field was formatted to occupy the maximum number
    of chars allowed. Other fields were kept unchanged.
    
    From now on, all fields are adjusted to occupy the whole length of
    the longest data in each field.
    
    Signed-off-by: Gabriel Vinicius Canzi Candido <gabriel.vccandido@gmail.com>

--- indent-host-file.pl
+++ indent-host-file.pl
@@ -5,10 +5,27 @@ use warnings;
 my $INPUT = '/tmp/output_1.txt';
 my $OUTPUT = '/tmp/output_2.txt';
 my $SEPARATOR = '   ';
+my $N_FIELDS = 10;
 
 open(my $IN_FILE, $INPUT) or die $!; # open(my $FILE, '<', $INPUT)
 open(my $OUT_FILE, '>', $OUTPUT) or die $!;
 
+my @column_sizes = (0) x $N_FIELDS;
+
+while (<$IN_FILE>) {
+	# split the line between data and comment and remove extra spaces
+	my @line = split(/#/); # split(/PATTERN/, $_)
+	my @data_line = split(/\s+/, $line[0]);
+
+	# check the size for each data field
+	for (my $col = 0; $col < $#data_line; $col++) {
+		if (length $data_line[$col] > $column_sizes[$col]) {
+			$column_sizes[$col] = length $data_line[$col];
+		}
+	}
+}
+
+seek($IN_FILE, 0, 0);
 while (<$IN_FILE>) {
 	# split the line between data and comment and remove extra spaces
 	my @line = split(/#/); # split(/PATTERN/, $_)
@@ -17,8 +34,12 @@ while (<$IN_FILE>) {
 	if (@data_line == 0) { # empty or comment line
 		print $OUT_FILE $_;
 	} else {
-		# IP field is at most 15 char long
-		$data_line[0] = sprintf("%-15s", $data_line[0]);
+		# format each field to the max size
+		for (my $col = 0; $col < $#data_line; $col++) {
+			my $size = $column_sizes[$col];
+			$data_line[$col] = 
+			        sprintf("%-${size}s", $data_line[$col]);
+		}
 
 		print $OUT_FILE join($SEPARATOR, @data_line);
 
